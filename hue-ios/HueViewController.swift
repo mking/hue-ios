72409ceb4315be0d12154fe7f283a53d6b09bb6b
Matthew King <mking@users.noreply.github.com>
2016-10-17 20:38:04 -0700

    init

---
+++ hue-ios/HueViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  hue-ios
+//
+//  Created by Matt King on 10/17/16.
+//  Copyright © 2016 Hue. All rights reserved.
+//
+
+import UIKit
+
+class HueViewController: UIViewController {
+    // MARK: UIViewController
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
